a670200ea8fc67240b86992b4d2c69d831090bf5
Mohd Zakwan <mohd.zakwan99@gmail.com>
2021-08-18 20:32:40 +0800

    chapter_7

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "static_pages#home"
+  get "static_pages/help"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
+end
