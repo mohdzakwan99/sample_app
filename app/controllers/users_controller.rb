a35ce28506f7b91ce62ad2e3f5cb719c96f81c9b
Mohd Zakwan <mohd.zakwan99@gmail.com>
2021-08-25 17:26:02 +0800

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:error] = "Error!"
+    redirect_to root_path
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("jquery")
+import "bootstrap"
+require("@rails/ujs").start()
+require("turbolinks").start()
 
 Rails.start()
 Turbolinks.start()
